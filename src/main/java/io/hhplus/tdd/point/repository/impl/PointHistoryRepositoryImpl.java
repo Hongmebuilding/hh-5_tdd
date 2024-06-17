b542ea71a787a8056478c0a6d0c8294974ed3780
sumin <survd0404@gmail.com>
2024-06-18 01:31:23 +0900

    add repository

---
+++ src/main/java/io/hhplus/tdd/point/repository/impl/PointHistoryRepositoryImpl.java
@@ -0,0 +1,25 @@
+package io.hhplus.tdd.point.repository.impl;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+@RequiredArgsConstructor
+public class PointHistoryRepositoryImpl implements PointHistoryRepository {
+    private final PointHistoryTable pointHistoryTable;
+
+    @Override
+    public PointHistory insert(long userId, long amount, TransactionType type, long updateMillis) {
+        return pointHistoryTable.insert(userId, amount, type, updateMillis);
+    }
+
+    @Override
+    public List<PointHistory> selectAllByUserId(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+}
