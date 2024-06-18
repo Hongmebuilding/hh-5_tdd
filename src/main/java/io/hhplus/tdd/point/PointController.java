f0b35b9ea0cdbedac9faf3fb40ed7769f2d202a4
sumin <survd0404@gmail.com>
2024-06-19 02:57:25 +0900

    feat: add controller & update controller test

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,56 +1,48 @@
 package io.hhplus.tdd.point;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
 
-    private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService service;
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}")
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        return service.getPoints(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return service.getPointHistories(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return service.chargePoints(id, amount);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/use")
     public UserPoint use(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return service.usePoints(id, amount);
     }
 }
--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -5,30 +5,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PointController.class)
 class PointControllerTest {
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Mock
+    @MockBean
     private PointService pointService;
 
     @InjectMocks
@@ -37,82 +38,81 @@ class PointControllerTest {
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(pointController).build();
     }
 
     @Test
     @DisplayName("특정 유저의 포인트를 조회하는 기능")
     void point() throws Exception {
-        // given
         long userId = 1L;
         long point = 2L;
         long updateMillis = 0L;
         UserPoint userPoint = new UserPoint(userId, point, updateMillis);
 
-        // when
         when(pointService.getPoints(userId)).thenReturn(userPoint);
 
-        // then
         mockMvc.perform(get("/point/{id}", userId))
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.point").value(point));
     }
 
     @Test
     @DisplayName("특정 유저의 포인트 충전/이용 내역을 조회하는 기능")
     void history() throws Exception {
-        // given
         long userId = 1L;
-        List<PointHistory> historyList = new ArrayList<>();
-        historyList.add(new PointHistory(1L, userId, 1L, TransactionType.CHARGE, 1L));
-        historyList.add(new PointHistory(2L, userId, 2L, TransactionType.USE, 2L));
-        historyList.add(new PointHistory(3L, userId, 3L, TransactionType.CHARGE, 3L));
+        List<PointHistory> historyList = List.of(
+                new PointHistory(1L, userId, 1L, TransactionType.CHARGE, 1L),
+                new PointHistory(2L, userId, 2L, TransactionType.USE, 2L),
+                new PointHistory(3L, userId, 3L, TransactionType.CHARGE, 3L)
+        );
 
-        // when
         when(pointService.getPointHistories(userId)).thenReturn(historyList);
 
-        // then
         mockMvc.perform(get("/point/{id}/histories", userId))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].userId").value(userId))
+                .andExpect(jsonPath("$[0].amount").value(1L))
+                .andExpect(jsonPath("$[1].id").value(2L))
+                .andExpect(jsonPath("$[1].userId").value(userId))
+                .andExpect(jsonPath("$[1].amount").value(2L))
+                .andExpect(jsonPath("$[2].id").value(3L))
+                .andExpect(jsonPath("$[2].userId").value(userId))
+                .andExpect(jsonPath("$[2].amount").value(3L));
     }
 
     @Test
     @DisplayName("특정 유저의 포인트를 충전하는 기능")
     void charge() throws Exception {
-        // given
         long userId = 1L;
         long amount = 2L;
-        long updateMillis = 0L;
-        UserPoint userPoint = new UserPoint(userId, amount, updateMillis);
+        UserPoint userPoint = new UserPoint(userId, amount, 0L);
         String content = objectMapper.writeValueAsString(amount);
 
-        // when
         when(pointService.chargePoints(userId, amount)).thenReturn(userPoint);
 
-        // then
-        mockMvc.perform(patch("/point/{id}/charge", userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
-        ).andExpect(status().isOk());
+        performPatchRequest(userId, "/charge", content, amount);
     }
 
     @Test
     @DisplayName("특정 유저의 포인트를 사용하는 기능")
     void use() throws Exception {
-        // given
         long userId = 1L;
         long amount = 1L;
-        long updateMillis = 0L;
-        UserPoint userPoint = new UserPoint(userId, amount, updateMillis);
+        UserPoint userPoint = new UserPoint(userId, amount, 0L);
         String content = objectMapper.writeValueAsString(amount);
 
-        // when
         when(pointService.usePoints(userId, amount)).thenReturn(userPoint);
 
-        // then
-        mockMvc.perform(patch("/point/{id}/use", userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
-        ).andExpect(status().isOk());
+        performPatchRequest(userId, "/use", content, amount);
+    }
+
+    private void performPatchRequest(long userId, String action, String content, long amount) throws Exception {
+        mockMvc.perform(patch("/point/{id}" + action, userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.point").value(amount));
     }
 }
